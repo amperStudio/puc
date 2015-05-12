f04a281ada23cf020e62f8fcd37e51ba8c16fadb
João Paulo <jp@amperstudio.com.br>
2015-05-11 21:55:13 -0300

    Commit 2.0
    Versão com conexão de banco azure

--- conf/conn.php
+++ conf/conn.php
@@ -1,15 +1,15 @@
 <?php
  function conn_mysql(){
-
-   $servidor = '186.233.184.19';
+//Database=pucAKpCXSI0Zx64K;Data Source=br-cdbr-azure-south-a.cloudapp.net;User Id=b8beb24bbb9ba3;Password=870fdc21
+   $servidor = 'br-cdbr-azure-south-a.cloudapp.net';
   /* $servidor = '127.0.0.1';*/
    $porta = 3306;
    //$porta = 3306;
-   $banco = "pucminas";
+   $banco = "pucAKpCXSI0Zx64K";
    //$usuario = "root";
-   $usuario = "jppuc";
+   $usuario = "b8beb24bbb9ba3";
    //Elias2014 root
-   $senha = "anaclara";
+   $senha = "870fdc21";
    
       $conn = new PDO("mysql:host=$servidor;
                       port=$porta;
